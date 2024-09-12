9d35b57b19fc94105e8204cec079c4a579d3ad65
bunnyreddy14 <22eg107b22@anurag.edu.in>
2024-09-12 13:51:26 +0530

    fst

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,6 +1,7 @@
 /* GET homepage */
 const index = (req, res) => {
-    res.render('index', { title: 'Express' });
-    }; module.exports = {
+    res.render('index', { title: 'express' });
+    };
+    module.exports = {
         index
         };
